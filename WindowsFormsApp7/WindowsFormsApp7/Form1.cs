f4b486552875103f8b83c81da91651dfb6be68da
AmiraslanSumgait <emiraslaneliyev45@gmail.com>
2021-08-22 19:34:22 +0400

    60% complete

--- WindowsFormsApp7/WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/WindowsFormsApp7/Form1.cs
@@ -12,7 +12,7 @@ namespace WindowsFormsApp7
 {
     public partial class cbx_Shapes : Form
     {
-        //update in this week insallah qaqas
+        //update in this week insallah qaqasss
         
         public Graphics Graphics { get; set; }
         public List<IFigure> Shapes { get; set; } = new List<IFigure>();
